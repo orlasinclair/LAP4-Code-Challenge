cc8326e6fd8b4a2a8393d986b86fd9c404065f9b
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-05-04 11:22:37 +0100

    created a form.py for url

---
+++ theproject/lazy_url/form.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from django import forms
+from .models import UrlData
+
+class UrlForm(forms.ModelForm):
+    class Meta:
+        model = UrlData
+        fields = ['long_url']
+        
--- theproject/lazy_url/models.py
+++ theproject/lazy_url/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class ShortUrls(models.Model):
+class UrlData(models.Model):
     long_url = models.URLField(max_length=200)
-    short_url = models.CharFiedl(max_length=15, unique=True)
+    short_url = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return f'{self.long_url} becomes {self.short_url}'
